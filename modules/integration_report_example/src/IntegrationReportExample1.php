7468d4b1cb03322e13df8b7273bf57140f658813
Alex Skrypnyk <alex@drevops.com>
2026-05-30 13:40:51 +1000

    Added 'overview()' controller tests, Jest coverage output to '.logs/coverage/jest', and CodeCov upload paths.

--- modules/integration_report_example/src/IntegrationReportExample1.php
+++ modules/integration_report_example/src/IntegrationReportExample1.php
@@ -15,6 +15,8 @@ use Symfony\Component\HttpFoundation\Response;
  * Class IntegrationReportExample1.
  *
  * Example for the IntegrationReport module.
+ *
+ * @codeCoverageIgnore
  */
 class IntegrationReportExample1 extends IntegrationReportBase {
 
