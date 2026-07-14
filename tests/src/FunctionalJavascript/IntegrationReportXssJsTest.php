900ee297ebc26b77b2b5a6fc952ad06413463e69
Alex Skrypnyk <alex@drevops.com>
2026-07-15 09:42:06 +1000

    Added RunTestsInSeparateProcesses attribute to test classes.

--- tests/src/FunctionalJavascript/IntegrationReportXssJsTest.php
+++ tests/src/FunctionalJavascript/IntegrationReportXssJsTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\integration_report\FunctionalJavascript;
 
+use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
 use PHPUnit\Framework\Attributes\Group;
 
 /**
@@ -12,6 +13,7 @@ use PHPUnit\Framework\Attributes\Group;
  * @group integration_report
  */
 #[Group('integration_report')]
+#[RunTestsInSeparateProcesses]
 class IntegrationReportXssJsTest extends IntegrationReportJsTestBase {
 
   /**
